3b01d2c39f66e32424cdab56ae6af487da0845b5
jmoreno2411 <97634678+jmoreno2411@users.noreply.github.com>
2022-02-16 21:14:56 +0100

    Adding exercise 2

--- app/src/main/java/es/upv/dadm/myquotesapp/activities/FavouriteActivity.java
+++ app/src/main/java/es/upv/dadm/myquotesapp/activities/FavouriteActivity.java
@@ -1,6 +1,11 @@
 package es.upv.dadm.myquotesapp.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,9 +14,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import es.upv.dadm.myquotesapp.R;
+import es.upv.dadm.myquotesapp.adapters.FavouriteQuotesAdapter;
+import es.upv.dadm.myquotesapp.pojo.Quotation;
 
 public class FavouriteActivity extends AppCompatActivity {
 
@@ -20,9 +31,22 @@ public class FavouriteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
 
-        final View.OnClickListener listener = v -> onClick();
+        //final View.OnClickListener listener = v -> onClick();
 
-        findViewById(R.id.b_author_information).setOnClickListener(listener);
+        RecyclerView recycler = findViewById(R.id.rv_favourite);
+
+        LinearLayoutManager manager = new LinearLayoutManager(
+                FavouriteActivity.this, RecyclerView.VERTICAL, false);
+        recycler.setLayoutManager(manager);
+
+        DividerItemDecoration divider = new DividerItemDecoration(this, manager.getOrientation());
+        recycler.addItemDecoration(divider);
+
+        List<Quotation> data = getMockQuotations();
+        FavouriteQuotesAdapter adapter = new FavouriteQuotesAdapter(data);
+        recycler.setAdapter(adapter);
+
+        //findViewById(R.id.b_author_information).setOnClickListener(listener);
     }
 
     public void onClick() {
@@ -38,4 +62,22 @@ public class FavouriteActivity extends AppCompatActivity {
         }
 
     }
+
+    public List<Quotation> getMockQuotations() {
+      List<Quotation> res = new ArrayList<Quotation>();
+
+      res.add(new Quotation("Moral excellence comes about as a result of habit. We become just by doing just acts, temperate by doing temperate acts, brave by doing brave acts.", "Aristotle"));
+      res.add(new Quotation("There is nothing like returning to a place that remains unchanged to find the ways in which you yourself have altered.", "Nelson Mandela"));
+      res.add(new Quotation("My advice to you is not to inquire why or whither, but just enjoy your ice cream while its on your plate — that's my philosophy.", "Wilder"));
+      res.add(new Quotation("Be a yardstick of quality. Some people aren't used to an environment where excellence is expected.", "Stephen Jobs"));
+      res.add(new Quotation("The world is round and the place which may seem like the end may also be the beginning. ", "Ivy Baker Priest"));
+      res.add(new Quotation("Every problem has a gift for you in its hands. ", "Richard Bach"));
+      res.add(new Quotation("Happiness is the reward we get for living to the highest right we know. ", "Richard Bach"));
+      res.add(new Quotation("It isn't what happens to us that causes us to suffer; it's what we say to ourselves about what happens.", "Pema Chodron"));
+      res.add(new Quotation("Strength does not come from physical capacity. It comes from an indomitable will.", "Gandhi"));
+      res.add(new Quotation("You learn to speak by speaking, to study by studying, to run by running, to work by working; in just the same way, you learn to love by loving. ", "Anatole France"));
+
+      return res;
+    }
+
 }
